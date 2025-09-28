2ce125a2f08691c149c8023c5a42148e6211a9f4
Nguyen Van Hung <hungnv@dft.vn>
2025-09-28 23:54:28 +0700

    fix build

---
+++ angular/src/environments/environment.devtest.ts
@@ -0,0 +1,25 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'MY_FRONTEND_BASE_URL';
+export const environment = {
+  production: true,
+  hmr: false,
+  application: {
+    baseUrl,
+    name: 'SmartRestaurant',
+    logoUrl: '/layout/images/logo-white.svg',
+  },
+  oAuthConfig: {
+    issuer: 'MY_BACKEND_BASE_URL' + '/',
+    redirectUri: location.origin,
+    clientId: 'MY_WEB_CLIENT_ID',
+    responseType: 'form',
+    scope: 'offline_access SmartRestaurant',
+  },
+  apis: {
+    default: {
+      url: 'MY_REMOTE_SERVICE_BASE_URL',
+      rootNamespace: 'SmartRestaurant',
+    },
+  },
+} as Environment;
